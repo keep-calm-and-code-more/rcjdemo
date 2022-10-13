04ef34d31b9d72654acaef71a94c9c1bffb4437f
robinfang <thefmajia@gmail.com>
2022-10-13 16:55:00 +0800

    first init

---
+++ src/main/java/com/example/rcjdemo/RcjdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.rcjdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcjdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RcjdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rcjdemo/RcjdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rcjdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcjdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
