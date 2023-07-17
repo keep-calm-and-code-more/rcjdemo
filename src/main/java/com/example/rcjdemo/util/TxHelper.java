4914998babb44cb9c028f7a9be2df035a1674ec8
brightestBoy <yafeng@iscas.ac.cn>
2023-07-17 16:03:34 +0800

    feat: 继续适配2.0.0

--- src/main/java/com/example/rcjdemo/util/TxHelper.java
+++ src/main/java/com/example/rcjdemo/util/TxHelper.java
@@ -1,36 +1,64 @@
 package com.example.rcjdemo.util;
 
 
+import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.io.resource.ClassPathResource;
 import cn.hutool.core.lang.Dict;
 import cn.hutool.core.util.IdUtil;
 import cn.hutool.json.JSONUtil;
 import com.rcjava.protos.Peer;
+import com.rcjava.tran.TranCreator;
 import com.rcjava.tran.impl.InvokeTran;
+import com.rcjava.util.CertUtil;
 import com.rcjava.util.KeyUtil;
+import lombok.Getter;
+import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 import org.bouncycastle.operator.OperatorCreationException;
 import org.bouncycastle.pkcs.PKCSException;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.StringReader;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
 
+@Getter
 public class TxHelper {
 
-    private  Peer.CertId certId;
+    private Peer.CertId certId;
     private PrivateKey privateKey;
 
+    public TranCreator superTranCreator;
+    public Peer.CertId superCertId;
+
     /**
-     *
+     * 构建superAdmin
+     */
+    public TxHelper() {
+        PrivateKey super_pri = CertUtil.genX509CertPrivateKey(new File("private/jks/951002007l78123233.super_admin.jks"), "super_admin", "951002007l78123233.super_admin").getPrivateKey();
+        superTranCreator = TranCreator.newBuilder().setPrivateKey(super_pri).setSignAlgorithm("SHA256withECDSA").build();
+        superCertId = Peer.CertId.newBuilder().setCreditCode("identity-net:951002007l78123233").setCertName("super_admin").build();
+    }
+
+    /**
      * @param creditCode
      * @param certName
-     * @param pkFile 放resources里
+     * @param pkFile     放resources里
      * @throws IOException
      * @throws OperatorCreationException
      * @throws PKCSException
      */
     public TxHelper(String creditCode, String certName, String pkFile) throws IOException, OperatorCreationException, PKCSException {
+        this();
         ClassPathResource resource = new ClassPathResource(pkFile);
         this.privateKey = KeyUtil.generatePrivateKey(new PEMParser(resource.getReader(Charset.defaultCharset())), "");
         this.certId = Peer.CertId.newBuilder()
@@ -41,14 +69,34 @@ public class TxHelper {
     }
 
     /**
-     *
-     * @param scName 智能合约名称
-     * @param scVer 智能合约版本
+     * @param creditCode
+     * @param certName
+     * @param pkFile
+     * @throws IOException
+     * @throws OperatorCreationException
+     * @throws PKCSException
+     */
+    public TxHelper(String creditCode, String certName, File pkFile) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        this();
+        // 以后优化吧
+        String pemString = FileUtil.readString(pkFile, StandardCharsets.UTF_8);
+        PEMParser stringParser = new PEMParser(new StringReader(pemString));
+        PrivateKey privateKey = new JcaPEMKeyConverter().setProvider(new BouncyCastleProvider()).getPrivateKey((PrivateKeyInfo) stringParser.readObject());
+        // 交易的签名算法根据对应RepChain版本进行设置
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(privateKey.getEncoded());
+        this.privateKey = KeyFactory.getInstance("EC").generatePrivate(keySpec);
+        this.certId = Peer.CertId.newBuilder().setCreditCode(creditCode).setCertName(certName).build(); // 签名ID
+
+    }
+
+    /**
+     * @param scName   智能合约名称
+     * @param scVer    智能合约版本
      * @param funcName 智能合约方法
-     * @param argDict 参数
+     * @param argDict  参数
      * @return
      */
-    public Peer.Transaction callArgTx(String scName, Integer scVer, String funcName, Dict argDict){
+    public Peer.Transaction callArgTx(String scName, Integer scVer, String funcName, Dict argDict) {
         Peer.ChaincodeId chaincodeId = Peer.ChaincodeId.newBuilder()
                 .setChaincodeName(scName)
                 .setVersion(scVer)
