9e9d990f2140def13155811a18fcbc8f54af0fd0
robinfang <thefmajia@gmail.com>
2024-01-29 17:34:49 +0800

    增加用户是否被禁用的获取方法

--- src/main/java/com/example/rcjdemo/common/BlockSync.java
+++ src/main/java/com/example/rcjdemo/common/BlockSync.java
@@ -137,6 +137,7 @@ public class BlockSync implements SyncListener {
 
                     //后续根据creditCode、certName、peerCert、peerSigner对本地数据库进行新增和修改等操作，
                     // 目的是让业务系统知道该用户已经注册到链上，并且业务系统可取得链上公钥证书，用于验证
+                    // 通过peerSigner.getSignerValid(); 可获取用户是否被禁用
                 }
 
                 if (tx.getType() == Peer.Transaction.Type.CHAINCODE_INVOKE && StrUtil.equals(tx.getCid().getChaincodeName(), scOfInterest)) {
